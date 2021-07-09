aaa9cd6ff2d0b9de80df024bf6fb9fee3fd53179
Luigi Lupini <luigi.lupini@gmail.com>
2021-07-09 16:20:02 +0200

    Included a Form component into React

--- src/App.js
+++ src/App.js
@@ -85,22 +85,19 @@ const App = () => {
 
   const handleSearchInput = (event) => setSearchTerm(event.target.value);
 
-  const handleSearchSubmit = () => setUrl(`${API_ENDPOINT}${searchTerm}`);
+  const handleSearchSubmit = (event) => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+    event.preventDefault();
+  };
 
   return (
     <div>
       <h1>Hacker Stories</h1>
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        isFocused
-        onInputChange={handleSearchInput}
-      >
-        <strong>Search:</strong>
-      </InputWithLabel>
-      <button type="button" disabled={!searchTerm} onClick={handleSearchSubmit}>
-        Submit
-      </button>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
       <hr />
       {stories.isError && <p>Oops something went wrong...</p>}
       {stories.isLoading ? (
@@ -111,6 +108,22 @@ const App = () => {
     </div>
   );
 };
+
+const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+);
 
 const InputWithLabel = ({
   id,
