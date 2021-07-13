93b817656cf82c7b70b5d76f712fc3b7c3714399
Luigi Lupini <luigi.lupini@gmail.com>
2021-07-13 12:32:28 +0200

    Introducing file structure for components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import axios from 'axios';
-import { ReactComponent as Check } from './check.svg';
+import { SearchForm } from './SearchForm';
+import { List } from './List';
 
 import styles from './App.module.css';
 
@@ -114,81 +115,6 @@ const App = () => {
   );
 };
 
-const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-  <form onSubmit={onSearchSubmit} className={styles.searchForm}>
-    <InputWithLabel
-      id="search"
-      value={searchTerm}
-      isFocused
-      onInputChange={onSearchInput}
-    >
-      Search:
-    </InputWithLabel>
-    <button
-      type="submit"
-      disabled={!searchTerm}
-      className={`${styles.button} ${styles.buttonLarge}`}
-    >
-      Submit
-    </button>
-  </form>
-);
-
-const InputWithLabel = ({
-  id,
-  value,
-  type = 'text',
-  onInputChange,
-  isFocused,
-  children,
-}) => (
-  <>
-    <label htmlFor={id} className="label">
-      {children}
-    </label>
-    &nbsp;
-    <input
-      id={id}
-      type={type}
-      value={value}
-      autoFocus={isFocused}
-      onChange={onInputChange}
-      className="input"
-    />
-  </>
-);
-
-const List = ({ list, onRemoveItem }) => (
-  <ul>
-    {list.map((item) => (
-      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
-    ))}
-  </ul>
-);
-
-const Item = ({ item, onRemoveItem }) => {
-  const { url, author, title, num_comments, points } = item;
-  return (
-    <li className={styles.item}>
-      <span style={{ width: '40%' }}>
-        <a href={url}>{title}</a>
-      </span>
-      <span style={{ width: '30%' }}>{author}</span>
-      <span style={{ width: '10%' }}>{num_comments}</span>
-      <span style={{ width: '10%' }}>{points}</span>
-      <span style={{ width: '10%' }}>
-        <button
-          type="button"
-          onClick={() => onRemoveItem(item)}
-          className={`${styles.button} ${styles.buttonSmall}`}
-        >
-          <Check height="18px" width="18px" />
-        </button>
-      </span>
-    </li>
-  );
-};
-
 export default App;
 
-export { storiesReducer, SearchForm, InputWithLabel, List, Item };
+export { storiesReducer, SearchForm, List };
